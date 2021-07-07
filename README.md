7e29460e08ce7462c0e55bb7eef21a1114270c7b
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-07-07 21:06:06 +0200

    Use pip. Add Readme Add .gitignore

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data Access Library
+Data access for applications represented using IDSs or CBS
+
+# Install
+`pip install .`
+
+# Requires
+1. python >= 3.8
+2. cachetools
+3. numpy
+4. proc
+5. logging2
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dataAccess",
+    version="0.0.1",
+    author="Abadie Lana",
+    author_email="lana.abadie@iter.org",
+    description="Data access for applications using IDSs or CBS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.iter.org/scm/vis/data-access.git",
+    project_urls={
+        "Bug Tracker": "https://jira.iter.org/issues/?jql=project%20%3D%20IDV%20AND%20component%20%3D%20Data-Access",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_namespace_packages(where="."),
+    python_requires=">=3.8",
+    install_requires=[
+        "cachetools >= 4.2.1",
+        "logging2 >= 0.0.1",
+        "numpy >= 1.20.0",
+        "proc >= 0.0.1"
+    ]
+)
