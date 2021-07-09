440e6a834626b76fe453332d3683b9fb9dd9eb86
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-07-09 13:26:29 +0200

    Use setuptools-git-version

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dataAccess",
-    version="0.1.0",
+    setup_requires=[
+        "setuptools-git-versioning"
+    ],
+    version_config={
+        "starting_version": "0.0.1",
+        "template": "{tag}",
+        "dirty_template": "{tag}.{ccount}.{sha}",
+    },
     author="Lana Abadie",
     author_email="lana.abadie@iter.org",
     description="Data access for applications using IDSs or CBS",
