f01580ba24c1ff64e009cbe92ecd7d2547fc9d73
Lana Abadie <lana.abadie@iter.org>
2022-09-26 12:38:47 +0200

    try uninstall sseclient-py first

--- environ.sh
+++ environ.sh
@@ -34,7 +34,7 @@ try module load iplotLogging/0.2.2-GCCcore-10.2.0
 
 # Testing/Coverage requirements
 try module load coverage/5.5-GCCcore-10.2.0
-try pip uninstall --no-input sseclient-py 
+try pip uninstall sseclient-py --yes
 case $toolchain in
 
   "foss")
