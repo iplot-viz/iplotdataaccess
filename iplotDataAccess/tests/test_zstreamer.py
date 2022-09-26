a358316bad1ba33b54c17bf6c1251e29474a8a68
Lana Abadie <lana.abadie@iter.org>
2022-09-26 14:08:30 +0200

    try uninstall sseclient-py first

--- iplotDataAccess/tests/test_zstreamer.py
+++ iplotDataAccess/tests/test_zstreamer.py
@@ -48,7 +48,7 @@ class TestRTAccess(unittest.TestCase):
         ##print(os.environ.get('DATASOURCESCONF'))
         ##with open('/tmp/mydataconf.cfg') as f:
         ##    print( f.readlines())
-        path1 = os.environ.get('PYTHONPATH')+":"+os.environ.get('PWD')+"/iplotDataAccess_intel/lib/python3.8/site-packages"
+        path1 = os.environ.get('PYTHONPATH')+":"+os.environ.get('PWD')+"/iplotDataAccess_intel/lib/python3.8/site-packages"+":"+os.environ.get('PWD')+"/iplotDataAccess_foss/lib/python3.8/site-packages"
         os.environ.update({'PYTHONPATH': path1})
         print(os.environ.get('PYTHONPATH'))
         if len(self.da.loadConfig()) < 1:
