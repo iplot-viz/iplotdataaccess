380a4167d0ee718830550251fddb6ed93298ffb6
Lana Abadie <lana.abadie@iter.org>
2022-09-26 09:35:07 +0200

    send log per mail

--- iplotDataAccess/tests/test_zstreamer.py
+++ iplotDataAccess/tests/test_zstreamer.py
@@ -6,7 +6,11 @@ from iplotDataAccess import dataCommon as dc
 import threading
 from telnetlib import Telnet
 import time
-import os
+import os, sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import smtplib
 # from iplotDataAccess import udaAccess as ua
 import iplotLogging.setupLogger as ls
 
@@ -53,6 +57,7 @@ class TestUDAAccess(unittest.TestCase):
 
 
     def test_Streamer(self) -> None:
+        f = open("/tmp/mylog", 'w')
         loopCnt=0
         ds = "codacuda"
         varname = ["UTIL-HV-S22-BUS1:TOTAL_POWER"]
@@ -73,35 +78,65 @@ class TestUDAAccess(unittest.TestCase):
                 time.sleep(0.1)
                 errcnt = errcnt + 1
                 print("data is null")
+                f.write("data is null ")
+                f.write("\n")
                 continue
             # we discard first point if too old
             if dobj.xdata[0] < ts:
-
+                f.write("found timestamp less current timestamp ")
+                f.write("\n")
                 print("found timestamp less than current timestamp %lu", dobj.xdata[0])
                 if len(dobj.xdata) == 1:
                     errcnt = errcnt + 1
-                    print("dobj is one")
-                    continue
+                    print("dobj has one element")
+                    f.write("dobj has one element")
+                    f.write("\n")
                 else:
                     if cnt == 0:
                         firstT = dobj.xdata[1]
                         print(" cnt=0 first timestamp %d", firstT)
+                        f.write("cnt=0 first timestamp")
+                        f.write("\n")
             else:
                 if cnt == 0:
                     firstT = dobj.xdata[0]
                     logger.info(" first timestamp %d", firstT)
+                    f.write("cnt=0 first timestamp, but timestamp is ok")
+                    f.write("\n")
             print("vname=%s timestamp %lu and val=%f", varname[0], dobj.xdata[0], dobj.ydata[0])
-
+            f.write("end of block")
+            f.write("\n")
             cnt = cnt + 1
 
         print("end of loop")
+        f.write("end of loop")
+        f.write("\n")
         self.da.stopSubscription(ds)
-
+        f.write("call to stop subscription")
+        f.write("\n")
         x.join(5)
 
         self.assertEqual(cnt, 6)
 
 
 if __name__ == "__main__":
+    mailServer = "SMTPX.iter.org"
+    fromaddr = "bamboo <no-reply@iter.org>"
+    toaddr="['lana.abadie@iter.org']"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = ", ".join(toaddr)
+    msg['Subject'] = "bamboo report"
+    body="log in case it hangs"
+    msg.attach(MIMEText(body, 'plain'))
     unittest.main()
     os.remove("/tmp/mydataconf.cfg")
+    with open("/tmp/mylog", "rb") as fil:
+        part = MIMEApplication(fil.read(), Name=os.path.basename(resultFile))
+
+    part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(resultFile)
+    msg.attach(part)
+    server = smtplib.SMTP(mailServer, 25)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
