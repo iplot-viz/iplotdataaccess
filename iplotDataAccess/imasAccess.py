c60e75e7360db00344f0b3284f421f8dc0030abb
Paulo Abreu <paulo.abreu@iter.org>
2024-08-29 14:03:21 +0200

    Reviewing imas/get_pulses to accept pattern. However, it is not processed and the whole routine needs to be reviewed.

--- iplotDataAccess/imasAccess.py
+++ iplotDataAccess/imasAccess.py
@@ -119,7 +119,7 @@ class IMASDataAccess:
     def is_connected(self):
         return self.__isConnected
 
-    def get_pulses(self, pulse='*', run='????'):
+    def get_pulses(self, pulse='*', run='????', **kwargs):
         run_path = '0'
         user = 'public'
         db = 'ITER'
